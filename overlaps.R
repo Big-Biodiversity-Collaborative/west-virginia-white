237b2de804e1af1a8d05e28659e837be34789b92
Jeff Oliver <jcoliver@email.arizona.edu>
2023-02-23 22:19:11 -0700

    Add outline for envelope approach

---
+++ overlaps.R
@@ -0,0 +1,19 @@
+# Investigate amount of overlap through time
+# Jeff Oliver
+# jcoliver@arizona.edu
+# 2023-02-23
+
+# Want to see how much (prop/perc) of West Virginia White observations overlap 
+# with native host plants.
+
+# Load in data
+
+# Determine latitude bands (0-25, 25-50, 50-75, 75-100) or (1/3, 2/3, 3/3)?
+
+# Calculate, for each year, each band 80% envelope (or 10, 90 percentiles)
+
+# Calculate for each year, each band, what proportion of the insect 80% falls 
+# within the plants' 80%
+
+# Plot the proportion of insects 80% by year for each latitude band
+
